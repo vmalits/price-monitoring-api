bfafbf73ffbbdc0701646c84f5746721bba44f95
Vladimir Malits <vladimir.malits@gmail.com>
2020-10-26 16:36:07 +0200

    added sanctum package

--- routes/api.php
+++ routes/api.php
@@ -9,5 +9,9 @@ Route::group([
 ], function () {
     Route::post('register', RegisterController::class)->name('register');
     Route::get('email/verify/{id}', [VerificationController::class, 'verify'])->name('verification.verify');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('email/resend', [VerificationController::class, 'resend'])->name('verification.resend');
+    });
+
 });
 

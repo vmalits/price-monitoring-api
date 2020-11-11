50428170c93cb84b2992bfda7bf64a1a895a75f3
Vladimir Malits <vladimir.malits@gmail.com>
2020-11-11 10:22:37 +0200

    added type hint

--- app/Exceptions/DontVerifiedEmailException.php
+++ app/Exceptions/DontVerifiedEmailException.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class DontVerifiedEmailException extends Exception
 {
-    public function render()
+    public function render(): JsonResponse
     {
         return response()
             ->json(

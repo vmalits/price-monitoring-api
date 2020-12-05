7f5f80e3976f77c41b12c0ccb31a0b211ee5a423
Vladimir Malits <vladimir.malits@gmail.com>
2020-12-05 20:10:29 +0200

    was written tests for RoleController

---
+++ database/factories/RoleFactory.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\Role;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RoleFactory extends Factory
+{
+    protected $model = Role::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+        ];
+    }
+}
